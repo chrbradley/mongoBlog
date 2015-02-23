6cf3f8e2076ab55ff8cc0aafe950609de8b97329
Chris Bradley <chrbradley@gmail.com>
2015-02-23 12:50:00 -0500

    add besic error handeling

---
+++ routes/errors.js
@@ -0,0 +1,25 @@
+module.exports = function(app) {
+
+  // 404s
+  app.use(function (req, res, next) {
+    res.status(404);
+
+    if (req.accepts('html')) {
+      return res.send("<h2>I'm sorry, but I couldn't find that page.</h2>");
+    }
+
+    if (req.accepts('json')) {
+      return res.json({error: 'Not Found'});
+    }
+
+    // default response type
+    res.type('txt');
+    res.send("Hmmm, I couldn't find that page. Sorry for the inconvience");
+  });
+
+  // 500
+  app.use(function (err, req, res, next) {
+    console.error('error at %s\n', req.url, err);
+    res.send(500, "Oops, we made a boo boo.");
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+var errors = require('./errors');
+
 module.exports = function(app) {
   // home page
   app.get('/', function(req, res) {
     res.render('home.jade');
   });
+
+  // error handlers
+  errors(app);
 };
